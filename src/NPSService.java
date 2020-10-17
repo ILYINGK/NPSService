07853bef98a2aa3d815d8a3194347bb378b9b716
ILYINGK <ilyin_gk@mail.ru>
2020-10-17 18:01:10 +0300

    оптимизированно

--- src/NPSService.java
+++ src/NPSService.java
@@ -4,17 +4,17 @@ public class NPSService {
 
         int detractors = 0;
         int promoters = 0;
-        int neutrals = 0;
+        //int neutrals = 0;
 
         for (int vote : votes) {
             if(vote<=6)detractors++;
             if (vote>=9)promoters++;
-            if(vote>6&&vote<9)neutrals++;
+            //if(vote>6&&vote<9)neutrals++;
         }
 
-        int sum = detractors+promoters+neutrals;
+        int abonents = votes.length;
 
-        double nps =  promoters*1.0/sum*100 - detractors*1.0/sum*100;
+        double nps =  promoters*1.0/abonents*100 - detractors*1.0/abonents*100;
 
         return (int) nps;
     }
